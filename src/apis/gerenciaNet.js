0841fc419f455698c39a02482ff3aca7b9444dd7
chavesjoabe <chavesjoabe1@gmail.com>
2021-05-21 00:54:59 -0300

    feat: refactor, creating gerenciaNet file

---
+++ src/apis/gerenciaNet.js
@@ -0,0 +1,51 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+
+const cert = fs.readFileSync(
+  path.resolve(__dirname, `../../certs/${process.env.GN_CERT}`)
+);
+
+const agent = new https.Agent({
+  pfx: cert,
+  passphrase: "",
+});
+
+const credentials = Buffer.from(
+  `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
+).toString("base64");
+
+const authenticate = async () => {
+  return await axios({
+    method: "POST",
+    url: `${process.env.GN_ENDPOINT}/oauth/token`,
+    headers: {
+      Authorization: `Basic ${credentials}`,
+      "Content-Type": "application/json",
+    },
+    httpsAgent: agent,
+    data: {
+      grant_type: "client_credentials",
+    },
+  });
+};
+
+const GNrequest = async () => {
+  const authResponse = await authenticate();
+  const accessToken = authResponse.data?.access_token;
+  return axios.create({
+    baseURL: process.env.GN_ENDPOINT,
+    httpsAgent: agent,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+module.exports = GNrequest;
